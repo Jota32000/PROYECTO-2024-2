a0aaf1cb671bb93f67f853e7c9385da59e3efa9f
BjnArancibia <benyamindragon@gmail.com>
2024-08-18 22:58:04 -0400

    Revert "Merge remote-tracking branch 'origin/Absalon' into Benjamin"
    This reverts commit efc7044ce5ec497d60c7b94f98f8ce6f638775dc, reversing
    changes made to 138a57541101a299953c148518e39e46b8087c07.

--- Protoboard/main.py
+++ Protoboard/main.py
@@ -1,20 +1,16 @@
 import pygame
-import math
 from pygame.locals import *
 
 class Conector:
-    def __init__(self, x, y, lista_conectores):  # agrege lista conectores
+    def __init__(self, x, y):
         self.x = x
         self.y = y
         self.largo = 5
         self.color = (84, 84, 84)
-        lista_conectores.append((self.x + 2, self.y + 2))  # agrega las cc a la lista
 
     def dibujar(self, screen):
         for i in range(self.largo):
             pygame.draw.line(screen, self.color, (self.x, self.y + i), (self.x + self.largo, self.y + i))
-
-conectores = []
 
 class Protoboard:
     def __init__(self, x, y):
@@ -120,28 +116,28 @@ class Protoboard:
             for j in range(num_columnas):
                 x_pos = inicio_x + j * separacion_x
                 y_pos = inicio_y + i * 20
-                conector = Conector(x_pos, y_pos,conectores)
+                conector = Conector(x_pos, y_pos)
                 conector.dibujar(screen)
 
         for i in range(2):
             for j in range(num_columnas):
                 x_pos = inicio_x + j * separacion_x
                 y_pos = inicio_y + i * 20
-                conector = Conector(x_pos, y_pos+self.ancho-64,conectores)
+                conector = Conector(x_pos, y_pos+self.ancho-64)
                 conector.dibujar(screen)
 
         for i in range(num_filas):
             y_pos = inicio_y + i * 20
             for j in range(num_columnas):
                 x_pos = inicio_x + j * separacion_x
-                conector = Conector(x_pos, y_pos+70,conectores)
+                conector = Conector(x_pos, y_pos+70)
                 conector.dibujar(screen)
 
         for i in range(num_filas):
             y_pos = inicio_y + i * 20
             for j in range(num_columnas):
                 x_pos = inicio_x + j * separacion_x
-                conector = Conector(x_pos, y_pos+210,conectores)
+                conector = Conector(x_pos, y_pos+210)
                 conector.dibujar(screen)
 
 class Pila:
@@ -153,8 +149,6 @@ class Pila:
         self.color_componentes_pila = (170, 170, 170)
         self.largo = 750
         self.ancho = 550
-        conector_pila1 = Conector(98, 235, conectores)
-        conector_pila2 = Conector(128, 235, conectores)
 
     def dibujarPila(self,screen):
 
@@ -187,100 +181,36 @@ class Pila:
 
         #Componente negativo (-)
 
-        pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 30, self.pila_y - 2), (self.pila_x + 30, self.pila_y - 16), 3)
-        pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 30, self.pila_y - 16), (self.pila_x + 40, self.pila_y - 16), 3)
-        pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 40, self.pila_y - 16), (self.pila_x + 40, self.pila_y - 2), 3)
+        pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 20, self.pila_y - 2), (self.pila_x + 20, self.pila_y - 16), 3)
+        pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 20, self.pila_y - 16), (self.pila_x + 30, self.pila_y - 16), 3)
+        pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 30, self.pila_y - 16), (self.pila_x + 30, self.pila_y - 2), 3)
         
         #Componente Positivo (+)
         
         pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 60, self.pila_y  - 2), (self.pila_x + 60, self.pila_y - 16), 3)
-        pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 60, self.pila_y - 16), (self.pila_x + 60, self.pila_y - 16), 3)
+        pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 60, self.pila_y - 16), (self.pila_x + 70, self.pila_y - 16), 3)
         pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 70, self.pila_y - 2), (self.pila_x + 70, self.pila_y - 16), 3)
         
         #Ciclo para rellenar componente 1
-        for i in range(10):
+        for i in range(9):
             pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 60, self.pila_y - 16), (self.pila_x + 60 + i, self.pila_y - 2), 3)
             pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 70, self.pila_y - 2), (self.pila_x + 70 - i, self.pila_y - 16), 3)
             
         #Ciclo para rellenar componente 2
-        for i in range(10):
-            pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 30, self.pila_y - 16), (self.pila_x + 30 + i, self.pila_y - 2), 3)
-            pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 40, self.pila_y - 2), (self.pila_x + 40 - i, self.pila_y - 16), 3)
+        for i in range(9):
+            pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 20, self.pila_y - 16), (self.pila_x + 20 + i, self.pila_y - 2), 3)
+            pygame.draw.line(screen, (self.color_componentes_pila), (self.pila_x + 30, self.pila_y - 2), (self.pila_x + 30 - i, self.pila_y - 16), 3)
 
         #Inclusión de positivo y negativo
 
         #Negativo
 
-        pygame.draw.line(screen, (self.color_cuerpo_pila), (self.pila_x + 40, self.pila_y + 15), (self.pila_x + 30, self.pila_y + 15), 2)
+        pygame.draw.line(screen, (self.color_cuerpo_pila), (self.pila_x + 30, self.pila_y + 15), (self.pila_x + 20, self.pila_y + 15), 2)
 
         #Positivo
 
         pygame.draw.line(screen, (self.color_cuerpo_pila), (self.pila_x + 70, self.pila_y + 15), (self.pila_x + 60, self.pila_y + 15), 2)
         pygame.draw.line(screen, (self.color_cuerpo_pila), (self.pila_x + 65, self.pila_y + 10), (self.pila_x + 65, self.pila_y + 20), 2)
-        
-        
-class Menu:
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y + 25
-        self.ancho = 10
-        self.alto = 15
-        self.color_fondo_led = (66, 214, 2)
-        self.color_fondo_cable_positivo = (255, 44, 44)
-        self.color_fondo_cable_negativo = (0, 90, 247)
-        self.color_led = (50, 164, 0)
-        self.color_cable_positivo = (205, 0, 0)
-        self.color_cable_negativo = (33, 0, 183)
-
-        #funcion para el menu de la led
-    def dibujar_menu_led(self, menu):
-
-        pygame.draw.line(screen, (self.color_fondo_led), (self.x + 450, self.y - 100), (self.x + 500, self.y - 100), 3)
-        pygame.draw.line(screen, (self.color_fondo_led), (self.x + 500, self.y - 100), (self.x + 500, self.y - 50), 3)
-        pygame.draw.line(screen, (self.color_fondo_led), (self.x + 500, self.y - 50), (self.x + 450, self.y - 50), 3)
-        pygame.draw.line(screen, (self.color_fondo_led), (self.x + 450, self.y - 50), (self.x + 450, self.y - 100), 3)
-
-        #Ciclo para rellenar
-        for i in range(50):
-            pygame.draw.line(screen, (self.color_fondo_led), (self.x + 450, self.y - 100), (self.x + 450 + i, self.y - 50), 3)
-            pygame.draw.line(screen, (self.color_fondo_led), (self.x + 500, self.y - 50), (self.x + 500 - i, self.y - 100), 3)
-                        
-    def dibujar_menu_cable_positivo(self,menu):
-        pygame.draw.line(screen, (self.color_fondo_cable_positivo), (self.x + 450, self.y - 25), (self.x + 500, self.y - 25), 3)
-        pygame.draw.line(screen, (self.color_fondo_cable_positivo), (self.x + 500, self.y - 25), (self.x + 500, self.y + 25), 3)
-        pygame.draw.line(screen, (self.color_fondo_cable_positivo), (self.x + 500, self.y + 25), (self.x + 450, self.y + 25), 3)
-        pygame.draw.line(screen, (self.color_fondo_cable_positivo), (self.x + 450, self.y + 25), (self.x + 450, self.y - 25), 3)
-
-        #Ciclo para rellenar
-        for i in range(50):
-            pygame.draw.line(screen, (self.color_fondo_cable_positivo), (self.x + 450, self.y + 25), (self.x + 500, self.y + 25 - i), 3)
-            pygame.draw.line(screen, (self.color_fondo_cable_positivo), (self.x + 500, self.y - 25), (self.x + 450, self.y - 25 + i), 3)
-
-    def dibujar_menu_cable_negativo(self,menu):
-        pygame.draw.line(screen, (self.color_fondo_cable_negativo), (self.x + 450, self.y + 50), (self.x + 500, self.y + 50), 3)
-        pygame.draw.line(screen, (self.color_fondo_cable_negativo), (self.x + 500, self.y + 50), (self.x + 500, self.y + 100), 3)
-        pygame.draw.line(screen, (self.color_fondo_cable_negativo), (self.x + 500, self.y + 100), (self.x + 450, self.y + 100), 3)
-        pygame.draw.line(screen, (self.color_fondo_cable_negativo), (self.x + 450, self.y + 100), (self.x + 450, self.y + 50), 3)
-
-        #Ciclo para rellenar
-        for i in range(50):
-            pygame.draw.line(screen, (self.color_fondo_cable_negativo), (self.x + 450, self.y + 100), (self.x + 500, self.y + 100 - i), 3)
-            pygame.draw.line(screen, (self.color_fondo_cable_negativo), (self.x + 500, self.y + 50), (self.x + 450, self.y + 50 + i), 3)      
-
-    def dibujar_led(self,menu):
-        pygame.draw.line(screen, (self.color_led), (self.x + 470, self.y - 75), (self.x + 470, self.y - 55), 3)        
-        pygame.draw.line(screen, (self.color_led), (self.x + 480, self.y - 75), (self.x + 480, self.y - 55), 3)
-        pygame.draw.line(screen, (self.color_led), (self.x + 460, self.y - 75), (self.x + 490, self.y - 75), 3)
-        pygame.draw.line(screen, (self.color_led), (self.x + 460, self.y - 75), (self.x + 460, self.y - 95), 3)
-        pygame.draw.line(screen, (self.color_led), (self.x + 460, self.y - 95), (self.x + 490, self.y - 95), 3)
-        pygame.draw.line(screen, (self.color_led), (self.x + 490, self.y - 95), (self.x + 490, self.y - 75), 3)
-
-    def dibujar_cable_positivo(self,menu):
-        pygame.draw.line(screen, (self.color_cable_positivo), (self.x + 490, self.y), (self.x + 460, self.y), 6)
-        pygame.draw.line(screen, (self.color_cable_positivo), (self.x + 475, self.y + 15), (self.x + 475, self.y - 15), 6)
-
-    def dibujar_cable_negativo(self,menu):
-        pygame.draw.line(screen, (self.color_cable_negativo), (self.x + 490, self.y + 75), (self.x + 460, self.y + 75), 6)
         
 def dibujar_a(screen, x, y,ancho,alto,color):
     pygame.draw.line(screen, color, (x, y + alto), (x + ancho // 2, y), 2)  # Línea diagonal izquierda
@@ -378,38 +308,6 @@ def dibujar_0(screen,x,y,alto,color):
     pygame.draw.line(screen, color, (x + alto, y+alto), (x + alto, y ), 2)
     pygame.draw.line(screen, color, (x, y), (x+alto, y), 2)
 
-class Cableado:
-    def __init__(self):
-        self.dibujando_cable = False
-        self.inicio_cable = None
-        self.cables = []
-
-    def dibujar_cables(self):
-        for cable in self.cables:
-            pygame.draw.line(screen,"black", cable[0], cable[1], 3)
-
-    def comienzo_cable(self, anclaje):
-        self.inicio_cable = anclaje
-        self.dibujando_cable = True
-
-    def finalizar_cable(self, anclaje):
-        if not self.quitar_cable(self.inicio_cable, anclaje):
-            self.cables.append((self.inicio_cable, anclaje))
-        self.dibujando_cable = False
-        self.inicio_cable = None
-
-    def quitar_cable(self, start, end):
-        for cable in self.cables:
-            if (cable[0] == start and cable[1] == end) or (cable[0] == end and cable[1] == start):
-                self.cables.remove(cable)
-                return True
-        return False
-
-
-    def dibujar_cable_actual(self):
-        if self.dibujando_cable and self.inicio_cable:
-            current_pos = pygame.mouse.get_pos()
-            pygame.draw.line(screen, "black", self.inicio_cable, current_pos, 3)
 
 
 
@@ -425,7 +323,6 @@ pygame.display.set_caption("Protoboard")
 mainClock = pygame.time.Clock()
 monitor_size = [pygame.display.Info().current_w, pygame.display.Info().current_h]
 
-cableado = Cableado()
 fullscreen = False
 running = True
 
@@ -437,7 +334,6 @@ while running:
 
     # Crear y dibujar Protoboard
 
-    conectores.clear()
     protoboard = Protoboard(x_proto, y_proto)
     protoboard.crear(screen)
 
@@ -449,40 +345,6 @@ while running:
     pila = Pila(x_pila, y_pila)
     pila.dibujarPila(screen)
 
-    # Crear y dibujar Menu
-
-    x_menu = (screen.get_width() - 200) // 2
-    y_menu = (screen.get_height() - 40) // 2
-
-    menu = Menu(x_menu, y_menu)
-    menu.dibujar_menu_led(screen)
-    menu.dibujar_menu_cable_positivo(screen)
-    menu.dibujar_menu_cable_negativo(screen)
-    menu.dibujar_led(screen)
-    menu.dibujar_cable_positivo(screen)
-    menu.dibujar_cable_negativo(screen)
-
-
-    def distancia(punto1, punto2):
-        return math.sqrt((punto1[0] - punto2[0]) ** 2 + (punto1[1] - punto2[1]) ** 2)
-
-
-    def punto_mas_cercano(pos_mouse, lista_conectores, distancia_maxima):
-        punto_cercano = None
-        distancia_minima = 10000
-
-        for conector in lista_conectores:
-            dist = distancia(pos_mouse, conector)
-            if dist < distancia_minima and dist <= distancia_maxima:
-                distancia_minima = dist
-                punto_cercano = conector
-        return punto_cercano
-
-
-    distancia_maxima = 10
-
-    cableado.dibujar_cables()
-    
     # Manejo de eventos
 
     for event in pygame.event.get():
@@ -506,19 +368,6 @@ while running:
                 else:
                     screen = pygame.display.set_mode((screen.get_width(), screen.get_height()), pygame.RESIZABLE)
 
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            mouse_pos = event.pos
-            conector_cercano = punto_mas_cercano(mouse_pos, conectores, distancia_maxima)
-
-            if conector_cercano:
-                x1, y1 = conector_cercano
-
-                if not cableado.dibujando_cable:
-                    cableado.comienzo_cable((x1, y1))
-                else:
-                    cableado.finalizar_cable((x1, y1))
-
-    cableado.dibujar_cable_actual()
     pygame.display.flip()
     mainClock.tick(60)
 
